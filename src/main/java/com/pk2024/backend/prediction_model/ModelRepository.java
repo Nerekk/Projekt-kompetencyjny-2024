025bbf05d0adde8c985bc00f185b9982b974d442
zurson <matu.jurek@gmail.com>
2024-05-22 17:39:43 +0200

    Removed unnecessary model related classes

--- src/main/java/com/pk2024/backend/prediction_model/ModelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pk2024.backend.prediction_model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModelRepository extends JpaRepository<ModelEntity, Integer> {
-}
