8dec4347f5cf6d0af6f3dceee85158efdf7689d5
thejerremi <thejerremi@gmail.com>
2024-05-22 02:55:55 +0200

    cors configuration

--- src/main/java/com/pk2024/backend/config/SecurityConfiguration.java
+++ src/main/java/com/pk2024/backend/config/SecurityConfiguration.java
@@ -12,6 +12,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static com.pk2024.backend.settings.Settings.AUTH_MAPPING;
 import static com.pk2024.backend.settings.Settings.LOGOUT_MAPPING;
@@ -68,5 +74,17 @@ public class SecurityConfiguration {
         ;
 
         return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Allow this origin
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
