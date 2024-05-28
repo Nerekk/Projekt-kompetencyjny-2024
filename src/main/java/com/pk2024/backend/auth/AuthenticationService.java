176587a2e7f2b33dbfebfa5a80f4d4b1c79f157b
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-28 23:27:16 +0200

    Remove of redundant fields from AuthenticationService

--- src/main/java/com/pk2024/backend/auth/AuthenticationService.java
+++ src/main/java/com/pk2024/backend/auth/AuthenticationService.java
@@ -32,20 +32,12 @@ public class AuthenticationService {
 
     public AuthenticationResponse register(RegisterRequest request) throws Exception {
         var user = User.builder()
-                .firstname(request.getFirstname())
-                .lastname(request.getLastname())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(Role.USER)
                 .build();
 
         User savedUser = repository.save(user);
-
-//        try {
-//            savedUser =
-//        } catch (Exception e) {
-//            throw new PSQLException(new ServerErrorMessage(e.getMessage()));
-//        }
 
         var jwtToken = jwtService.generateToken(user);
         var refreshToken = jwtService.generateRefreshToken(user);
