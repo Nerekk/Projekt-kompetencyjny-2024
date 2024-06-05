ecd400f2882f8a6667078cafc176ff8b92bc0f13
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-05 17:54:59 +0200

    Register input validation

--- src/main/java/com/pk2024/backend/auth/RegisterRequest.java
+++ src/main/java/com/pk2024/backend/auth/RegisterRequest.java
@@ -1,5 +1,7 @@
 package com.pk2024.backend.auth;
 
+import com.pk2024.backend.settings.Settings;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +13,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be null or blank")
     private String email;
+
+    @NotBlank(message = "Password cannot be null or blank")
+    @Size(min = Settings.PASSWORD_MIN_SIZE, message = "Given password is too short")
     private String password;
 }
